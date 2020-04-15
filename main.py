c7294d31e3b3430fe0dc3bd76bc37a5decf238f1
angelina-tsuboi <angelina.t1832@gmail.com>
2020-04-14 20:55:23 -0700

    Added more functionality

--- main.py
+++ main.py
@@ -9,15 +9,20 @@ import webbrowser
 import smtplib
 
 print("This is wokring")
+chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
 # Setting Voices
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-voice = engine.setProperty('voice', voices[0])
+voice = engine.setProperty('voice', voices[3])
 # Setting Volume 
 volume = engine.getProperty('volume')  
-engine.setProperty('volume',1)   
+engine.setProperty('volume',1) 
+#Setting rate
+engine.setProperty('rate',180)   
 
 master = "Angelina"
+quit = False
+exit_commands = ["quit", "stop", "I want to leave", "bye", "exit", "leave"]
 
 def speak(text):
     engine.say(text)
@@ -79,15 +84,45 @@ def takeCommand():
     except Exception as e:
         print("Say that again?")
 
+def checkWikipedia(topic):
+    results = wikipedia.summary(topic, sentences=2)
+    speak(results)
+
+
 greeting()
-query = takeCommand()
-print(query)
+while(quit != True):
+    query = takeCommand()
+    print(query)
+    for command in exit_commands:
+        if command in query.lower():
+            print("Goodbye " + master)
+            speak("Goodbye " + master)
+            quit = True
+    if quit != True:
+        if 'wikipedia' in query.lower():
+            speak("Searching Wikipedia")
+            query = query.replace('wikipedia', '')
+            checkWikipedia(query)
+        elif 'time' in query.lower():
+            sayTime()
+        elif 'date' in query.lower():
+            sayFullDate()
+        elif 'what is' in query.lower():
+            speak("Searching Wikipedia")
+            query = query.replace('what is', '')
+            checkWikipedia(query)
+        elif 'what are' in query.lower():
+            speak("Searching Wikipedia")
+            query = query.replace('what are', '')
+            checkWikipedia(query)
+        elif 'open youtube' in query.lower() or 'open up youtube' in  query.lower():
+            print("Opening Youtube...")
+            webbrowser.get(chrome_path).open("http://youtube.com")
+        elif 'open school tabs' in query.lower() or 'open up school tabs' in query.lower():
+            webbrowser.get(chrome_path).open("https://docs.google.com/spreadsheets/d/1DOGhlYhpY8onx8mgolAUQGntmOXbwJNbwdaYZXq0txs/edit#gid=1691470956")
+            webbrowser.get(chrome_path).open("https://d15k2d11r6t6rl.cloudfront.net/public/users/Integrators/5eb55a21-9496-46ce-8161-f092fc9def23/chadwick/Middle%20School%20Distance%20Learning%20Schedule.pdf")
+            webbrowser.get(chrome_path).open("https://classroom.google.com/u/0/h")
+        else:
+            speak("Sorry, I didn't understand that")
 
-if 'wikipedia' in query or 'Wikipedia' in query:
-    print("Ummmm")
-
-if 'time' in query or 'Time' in query:
-    sayTime()
-
-if 'date' in query or 'Date' in query:
-    sayFullDate()
+    
