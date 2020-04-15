b911e7532610d19c42adb042ef9274b4943d4b04
angelina-tsuboi <angelina.t1832@gmail.com>
2020-04-14 17:03:42 -0700

    Got voice recognition to work

--- main.py
+++ main.py
@@ -15,11 +15,25 @@ voices = engine.getProperty('voices')
 voice = engine.setProperty('voice', voices[0])
 # Setting Volume 
 volume = engine.getProperty('volume')  
-engine.setProperty('volume',0.5)    
+engine.setProperty('volume',1)   
 
-def speak(audio):
-    engine.say(audio)
+master = "Angelina"
+
+def speak(text):
+    engine.say(text)
     engine.runAndWait()
+
+def greeting():
+    hour = int(datetime.now().hour)
+
+    if hour >= 0 and hour < 12:
+        speak("Good Morning " + master + " what can I do for you?")
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon " + master + " what can I do for you?")
+    else:
+        speak("Good Evening " + master + " what can I do for you?")
+
+    
 
 def sayFullDate():
     today = date.today()
@@ -27,8 +41,8 @@ def sayFullDate():
     now = now.split()
     print(now)
     print(now[2][:2])
-    engine.say("Today is " + str(now[0]) + str(now[1]) + str(now[2][:2]) + " " + str(now[2][2:]))
-    engine.runAndWait()
+    speak("Today is " + str(now[0]) + str(now[1]) + str(now[2][:2]) + " " + str(now[2][2:]))
+    
 
 def sayTime():
     now = datetime.now()
@@ -40,16 +54,40 @@ def sayTime():
     sec = dt[2]
     if int(hour) < 10:
         hour = hour[1]
-        
+    if int(hour) > 12:
+        hour = int(hour) % 12
+
     if int(min) < 10:
         print("here")
-        engine.say("It is " + str(hour) + " o " + str(min))
+        speak("It is " + str(hour) + " o " + str(min))
     else:
         print("ceer")
-        engine.say("It is " + str(hour) + " " + str(min))
-    engine.runAndWait()
+        speak("It is " + str(hour) + " " + str(min))
 
 def getTimeIn(city):
     pass
 
-sayTime()
+def takeCommand():
+    r = sr.Recognizer() 
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        return r.recognize_google(audio, language='en-in')
+        print("You said : {}".format(query))
+    except Exception as e:
+        print("Say that again?")
+
+greeting()
+query = takeCommand()
+print(query)
+
+if 'wikipedia' in query or 'Wikipedia' in query:
+    print("Ummmm")
+
+if 'time' in query or 'Time' in query:
+    sayTime()
+
+if 'date' in query or 'Date' in query:
+    sayFullDate()
